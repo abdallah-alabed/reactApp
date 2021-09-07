8d31198206415471fa4efcc3b72befe767dcd9ff
abdallah-alabed <abd.alabed97@gmail.com>
2021-09-07 18:28:04 +0300

    cards+grid done

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React, { Component } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import SelectedBeast from "./components/SelectedBeast";
-import HornedBeasts from "./components/HornedBeasts";
+// import HornedBeasts from "./components/HornedBeasts";
 import data from "./components/data.json";
 
 class App extends Component {
@@ -35,13 +36,12 @@ class App extends Component {
     return (
       <div>
         <Header />
-        <Main opening={this.opening} />
+        <Main opening={this.opening} data={this.state.data} />
         <SelectedBeast
           closing={this.closing}
           showInfo={this.state.showInfo}
           beastInfo={this.state.selectedBeast}
         />
-        <HornedBeasts opening={this.opening} />
         <Footer />
       </div>
     );
